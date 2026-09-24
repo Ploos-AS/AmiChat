e698d9686ba43152d784608cc37174126470185a
pos <pgousdal@users.noreply.github.com>
2026-09-24 04:39:01 +0200

    M1: add native Intuition GadTools GUI skeleton

---
+++ include/amichat_amiga_gui.h
@@ -0,0 +1,15 @@
+#ifndef AMICHAT_AMIGA_GUI_H
+#define AMICHAT_AMIGA_GUI_H
+#include "amichat_session.h"
+#ifdef __cplusplus
+extern "C" {
+#endif
+typedef struct AmiChatAmigaGUI AmiChatAmigaGUI;
+AmiChatAmigaGUI *AmiChat_AmigaGUIOpen(AmiChatSession *session);
+void AmiChat_AmigaGUIClose(AmiChatAmigaGUI *gui);
+unsigned long AmiChat_AmigaGUISignalMask(const AmiChatAmigaGUI *gui);
+int AmiChat_AmigaGUIHandle(AmiChatAmigaGUI *gui);
+#ifdef __cplusplus
+}
+#endif
+#endif
