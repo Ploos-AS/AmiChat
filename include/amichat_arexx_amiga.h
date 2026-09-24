52894c7242bd3c03a16411bfbda9caa9e2ec9f0c
pos <pgousdal@users.noreply.github.com>
2026-09-24 11:42:06 +0200

    M1: give native ARexx port async worker access

--- include/amichat_arexx_amiga.h
+++ include/amichat_arexx_amiga.h
@@ -1,11 +1,12 @@
 #ifndef AMICHAT_AREXX_AMIGA_H
 #define AMICHAT_AREXX_AMIGA_H
 #include "amichat_session.h"
+#include "amichat_worker_amiga.h"
 #ifdef __cplusplus
 extern "C" {
 #endif
 typedef struct AmiChatARexxPort AmiChatARexxPort;
-AmiChatARexxPort *AmiChat_ARexxPortOpen(AmiChatSession *session);
+AmiChatARexxPort *AmiChat_ARexxPortOpen(AmiChatSession *session,AmiChatAmigaWorker *worker);
 void AmiChat_ARexxPortClose(AmiChatARexxPort *port);
 int AmiChat_ARexxPortPoll(AmiChatARexxPort *port);
 unsigned long AmiChat_ARexxPortSignalMask(const AmiChatARexxPort *port);
