4457bb5436064bd36a374b54f45c27480ca994fe
pos <pgousdal@users.noreply.github.com>
2026-09-24 04:46:10 +0200

    M1: add transcript rebuild API

--- include/amichat_transcript.h
+++ include/amichat_transcript.h
@@ -1,6 +1,7 @@
 #ifndef AMICHAT_TRANSCRIPT_H
 #define AMICHAT_TRANSCRIPT_H
 #include "amichat.h"
+#include "amichat_message.h"
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -10,6 +11,8 @@ void AmiChat_TranscriptDestroy(AmiChatTranscript *transcript);
 AmiChatResult AmiChat_TranscriptBegin(AmiChatTranscript *transcript,AmiChatRole role);
 AmiChatResult AmiChat_TranscriptAppend(AmiChatTranscript *transcript,const char *data,size_t size);
 void AmiChat_TranscriptFinish(AmiChatTranscript *transcript);
+void AmiChat_TranscriptClear(AmiChatTranscript *transcript);
+AmiChatResult AmiChat_TranscriptRebuild(AmiChatTranscript *transcript,const AmiChatConversation *conversation);
 const char *AmiChat_TranscriptText(const AmiChatTranscript *transcript);
 size_t AmiChat_TranscriptLength(const AmiChatTranscript *transcript);
 int AmiChat_TranscriptActive(const AmiChatTranscript *transcript);
