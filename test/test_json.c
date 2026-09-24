8bfc9a65ac73513f82d94df0f9d70b58a80e060d
pos <pgousdal@users.noreply.github.com>
2026-09-24 17:10:23 +0200

    test: fix JSON string escaping fixture

--- test/test_json.c
+++ test/test_json.c
@@ -9,7 +9,7 @@ int main(void)
 
     buffer[0] = '\0';
     assert(AmiChat_JSONAppendString(buffer, sizeof(buffer), &length,
-                                    "hello "Amiga"\n") == AMICHAT_OK);
-    assert(strcmp(buffer, ""hello \\"Amiga\\"\\n"") == 0);
+                                    "hello \"Amiga\"\n") == AMICHAT_OK);
+    assert(strcmp(buffer, "\"hello \\\"Amiga\\\"\\n\"") == 0);
     return 0;
 }
