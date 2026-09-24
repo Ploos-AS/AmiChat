f789e9a7341e7ad8bdf70297c372b4a6b5e0e282
pos <pgousdal@users.noreply.github.com>
2026-09-24 04:28:43 +0200

    M1: expose conversation message accessors

--- include/amichat_message.h
+++ include/amichat_message.h
@@ -1,25 +1,18 @@
 #ifndef AMICHAT_MESSAGE_H
 #define AMICHAT_MESSAGE_H
-
 #include "amichat.h"
-
 #ifdef __cplusplus
 extern "C" {
 #endif
-
 typedef struct AmiChatMessage AmiChatMessage;
-
-AmiChatMessage *AmiChat_MessageCreate(AmiChatRole role, const char *content);
+AmiChatMessage *AmiChat_MessageCreate(AmiChatRole role,const char *content);
 void AmiChat_MessageFree(AmiChatMessage *message);
 AmiChatRole AmiChat_MessageRole(const AmiChatMessage *message);
 const char *AmiChat_MessageContent(const AmiChatMessage *message);
-
-AmiChatResult AmiChat_ConversationAppend(
-    AmiChatConversation *conversation,
-    AmiChatMessage *message);
-
+AmiChatResult AmiChat_ConversationAppend(AmiChatConversation *conversation,AmiChatMessage *message);
+size_t AmiChat_ConversationMessageCount(const AmiChatConversation *conversation);
+const AmiChatMessage *AmiChat_ConversationMessageAt(const AmiChatConversation *conversation,size_t index);
 #ifdef __cplusplus
 }
 #endif
-
 #endif
