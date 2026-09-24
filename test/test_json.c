d6cff1a5e769bda3ecd4954d7be43d99f7c3e5e8
pos <pgousdal@users.noreply.github.com>
2026-09-24 04:22:53 +0200

    M1: add message model and lightweight JSON support

---
+++ test/test_json.c
@@ -0,0 +1,15 @@
+#include "amichat_json.h"
+#include <assert.h>
+#include <string.h>
+
+int main(void)
+{
+    char buffer[128];
+    size_t length = 0;
+
+    buffer[0] = '\0';
+    assert(AmiChat_JSONAppendString(buffer, sizeof(buffer), &length,
+                                    "hello "Amiga"\n") == AMICHAT_OK);
+    assert(strcmp(buffer, ""hello \\"Amiga\\"\\n"") == 0);
+    return 0;
+}
