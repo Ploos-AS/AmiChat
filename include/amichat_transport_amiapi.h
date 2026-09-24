73a535b795dcb468a8dcd1fc8dee330864e07d55
pos <pgousdal@users.noreply.github.com>
2026-09-24 04:26:18 +0200

    M1: add concrete AmiAPI transport adapter

---
+++ include/amichat_transport_amiapi.h
@@ -0,0 +1,11 @@
+#ifndef AMICHAT_TRANSPORT_AMIAPI_H
+#define AMICHAT_TRANSPORT_AMIAPI_H
+#include "amichat_transport.h"
+#ifdef __cplusplus
+extern "C" {
+#endif
+AmiChatTransport *AmiChat_AmiAPITransportCreate(void);
+#ifdef __cplusplus
+}
+#endif
+#endif
