5eef5ee60b2552b650f9b00e269d0cd0407028e9
pos <pgousdal@users.noreply.github.com>
2026-09-24 04:41:53 +0200

    M1: connect GUI API to async worker

--- include/amichat_amiga_gui.h
+++ include/amichat_amiga_gui.h
@@ -1,11 +1,12 @@
 #ifndef AMICHAT_AMIGA_GUI_H
 #define AMICHAT_AMIGA_GUI_H
 #include "amichat_session.h"
+#include "amichat_worker_amiga.h"
 #ifdef __cplusplus
 extern "C" {
 #endif
 typedef struct AmiChatAmigaGUI AmiChatAmigaGUI;
-AmiChatAmigaGUI *AmiChat_AmigaGUIOpen(AmiChatSession *session);
+AmiChatAmigaGUI *AmiChat_AmigaGUIOpen(AmiChatSession *session,AmiChatAmigaWorker *worker);
 void AmiChat_AmigaGUIClose(AmiChatAmigaGUI *gui);
 unsigned long AmiChat_AmigaGUISignalMask(const AmiChatAmigaGUI *gui);
 int AmiChat_AmigaGUIHandle(AmiChatAmigaGUI *gui);
