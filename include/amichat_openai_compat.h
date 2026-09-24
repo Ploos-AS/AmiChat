391b482bc738c04731fe20958c5d2c8daaacac78
pos <pgousdal@users.noreply.github.com>
2026-09-24 04:24:40 +0200

    M1: expose OpenAI compatible chat request

--- include/amichat_openai_compat.h
+++ include/amichat_openai_compat.h
@@ -1,26 +1,15 @@
 #ifndef AMICHAT_OPENAI_COMPAT_H
 #define AMICHAT_OPENAI_COMPAT_H
-
 #include "amichat.h"
 #include "amichat_transport.h"
-
+#include "amichat_request.h"
 #ifdef __cplusplus
 extern "C" {
 #endif
-
-typedef struct AmiChatOpenAICompatConfig {
-    const char *base_url;
-    const char *api_key;
-    const char *organization;
-} AmiChatOpenAICompatConfig;
-
-AmiChatResult AmiChat_OpenAICompat_Init(
-    AmiChatProvider *provider,
-    AmiChatTransport *transport,
-    const AmiChatOpenAICompatConfig *config);
-
+typedef struct AmiChatOpenAICompatConfig { const char *base_url; const char *api_key; const char *organization; } AmiChatOpenAICompatConfig;
+AmiChatResult AmiChat_OpenAICompat_Init(AmiChatProvider *,AmiChatTransport *,const AmiChatOpenAICompatConfig *);
+AmiChatResult AmiChat_OpenAICompat_Chat(AmiChatProvider *,const AmiChatRequest *,AmiChatStream *);
 #ifdef __cplusplus
 }
 #endif
-
 #endif
