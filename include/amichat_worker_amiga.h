fa41e02e09a9250186d958d07709e7947ecd4f27
pos <pgousdal@users.noreply.github.com>
2026-09-24 04:41:12 +0200

    M1: add Amiga Exec worker message bridge

---
+++ include/amichat_worker_amiga.h
@@ -0,0 +1,18 @@
+#ifndef AMICHAT_WORKER_AMIGA_H
+#define AMICHAT_WORKER_AMIGA_H
+#include "amichat_session.h"
+#include "amichat_worker.h"
+#ifdef __cplusplus
+extern "C" {
+#endif
+typedef struct AmiChatAmigaWorker AmiChatAmigaWorker;
+AmiChatAmigaWorker *AmiChat_AmigaWorkerCreate(AmiChatSession *session);
+void AmiChat_AmigaWorkerDestroy(AmiChatAmigaWorker *worker);
+AmiChatResult AmiChat_AmigaWorkerSend(AmiChatAmigaWorker *worker,const char *prompt);
+void AmiChat_AmigaWorkerCancel(AmiChatAmigaWorker *worker);
+unsigned long AmiChat_AmigaWorkerSignalMask(const AmiChatAmigaWorker *worker);
+int AmiChat_AmigaWorkerDrain(AmiChatAmigaWorker *worker,AmiChatWorkerEventFn callback,void *userdata);
+#ifdef __cplusplus
+}
+#endif
+#endif
