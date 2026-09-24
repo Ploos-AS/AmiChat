8747b60b6da0acbd216947d0b028060de64f61bc
pos <pgousdal@users.noreply.github.com>
2026-09-24 04:24:36 +0200

    M1: add JSON response lookup

--- include/amichat_json.h
+++ include/amichat_json.h
@@ -1,21 +1,12 @@
 #ifndef AMICHAT_JSON_H
 #define AMICHAT_JSON_H
-
 #include "amichat.h"
-
 #ifdef __cplusplus
 extern "C" {
 #endif
-
-/* Small JSON string encoder used by provider adapters. */
-AmiChatResult AmiChat_JSONAppendString(
-    char *buffer,
-    size_t capacity,
-    size_t *length,
-    const char *value);
-
+AmiChatResult AmiChat_JSONAppendString(char *buffer,size_t capacity,size_t *length,const char *value);
+const char *AmiChat_JSONFindString(const char *json,const char *key,char *buffer,size_t capacity);
 #ifdef __cplusplus
 }
 #endif
-
 #endif
