3237dbf25bb4486c1cf827d8323dba783d7d60e3
pos <pgousdal@users.noreply.github.com>
2026-09-24 20:22:24 +0200

    test: commit valid JSON in SSE fixture

--- test/test_sse.c
+++ test/test_sse.c
@@ -19,10 +19,10 @@ static int cb(const char *d, size_t n, int final, void *userdata)
 
 int main(void)
 {
-    static const char first[] = "data: {\\\"delta\\\":{\\\"content\\\":\\\"Hel";
+    static const char first[] = "data: {\"delta\":{\"content\":\"Hel";
     static const char second[] =
-        "lo\\\"}}\n\n"
-        "data: {\\\"delta\\\":{\\\"content\\\":\\\" Amiga\\\"}}\n\n"
+        "lo\"}}\n\n"
+        "data: {\"delta\":{\"content\":\" Amiga\"}}\n\n"
         "data: [DONE]\n\n";
     AmiChatSSEParser parser;
     AmiChatStream stream;
