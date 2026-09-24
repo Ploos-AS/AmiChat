f712c289b65c484b0c59230b73b7dc4dd0101bb3
pos <pgousdal@users.noreply.github.com>
2026-09-24 04:25:22 +0200

    M1: add incremental SSE streaming parser

---
+++ include/amichat_sse.h
@@ -0,0 +1,16 @@
+#ifndef AMICHAT_SSE_H
+#define AMICHAT_SSE_H
+#include "amichat.h"
+#include "amichat_stream.h"
+#ifdef __cplusplus
+extern "C" {
+#endif
+#define AMICHAT_SSE_BUFFER_SIZE 8192
+typedef struct AmiChatSSEParser { char buffer[AMICHAT_SSE_BUFFER_SIZE]; size_t length; int done; AmiChatStream *stream; } AmiChatSSEParser;
+void AmiChat_SSEInit(AmiChatSSEParser *parser,AmiChatStream *stream);
+AmiChatResult AmiChat_SSEFeed(AmiChatSSEParser *parser,const char *data,size_t length);
+AmiChatResult AmiChat_SSEFinish(AmiChatSSEParser *parser);
+#ifdef __cplusplus
+}
+#endif
+#endif
