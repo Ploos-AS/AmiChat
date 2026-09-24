e833f63f09bf602c433848e7d89d345f9a60e2f5
pos <pgousdal@users.noreply.github.com>
2026-09-24 04:22:10 +0200

    M1: define provider instance structure

--- include/amichat_provider.h
+++ include/amichat_provider.h
@@ -6,7 +6,14 @@ extern "C" {
 #endif
 typedef struct AmiChatProviderInfo { const char *id; const char *name; unsigned long capabilities; } AmiChatProviderInfo;
 enum { AMICHAT_CAP_STREAMING=1UL<<0, AMICHAT_CAP_VISION=1UL<<1, AMICHAT_CAP_TOOLS=1UL<<2, AMICHAT_CAP_ATTACHMENTS=1UL<<3, AMICHAT_CAP_REASONING=1UL<<4 };
-typedef struct AmiChatProviderOps { AmiChatResult (*configure)(AmiChatProvider *, const char *); AmiChatResult (*list_models)(AmiChatProvider *); AmiChatResult (*send)(AmiChatProvider *, AmiChatConversation *); AmiChatResult (*cancel)(AmiChatProvider *); void (*destroy)(AmiChatProvider *); } AmiChatProviderOps;
+typedef struct AmiChatProviderOps {
+    AmiChatResult (*configure)(AmiChatProvider *, const char *);
+    AmiChatResult (*list_models)(AmiChatProvider *);
+    AmiChatResult (*send)(AmiChatProvider *, AmiChatConversation *);
+    AmiChatResult (*cancel)(AmiChatProvider *);
+    void (*destroy)(AmiChatProvider *);
+} AmiChatProviderOps;
+struct AmiChatProvider { const AmiChatProviderOps *ops; void *userdata; };
 #ifdef __cplusplus
 }
 #endif
